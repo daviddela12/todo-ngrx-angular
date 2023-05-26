509da7b93cbec2bbc252db4c21ef249574a1ca0c
ddl <ddl@novatec-gmbh.de>
2023-05-26 18:09:15 +0200

    chore: Add todos example commented

--- src/app/store/todo/todo.reducers.ts
+++ src/app/store/todo/todo.reducers.ts
@@ -1,6 +1,26 @@
 import {createFeature, createReducer, on} from "@ngrx/store";
 import {TodoActions} from "./todo.actions";
 import {initialState} from "./todo.state";
+import {Todo} from "./todo.model";
+/**
+const todosExample: Todo[] = [
+  {
+    id: 1,
+    text: "Go to the supermarket",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Update my skill profile",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Call mom",
+    completed: true
+  }
+]
+**/
 
 export const todoFeature = createFeature({
   name: "todo",
